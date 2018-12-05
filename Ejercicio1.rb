d109b66ea9741d84b9ac3c3a5397e0aae94311e5
aleion <alejandro.ion.carvajal@gmail.com>
2018-12-05 15:14:33 -0300

    Ejercicio 1 Listo

--- Ejercicio1.rb
+++ Ejercicio1.rb
@@ -1,1 +1,18 @@
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+uno = a.map {|i| i += 1}
+dos = a.map {|i| i.to_f}
+tres = a.map {|i| i.to_s }
+cuatro = a.reject {|i| i < 5 }
+cinco = a.select {|i| i < 5 }
+seis = a.inject {|sum, i| sum + i}
+siete = a.group_by {|i| i.even?}
+ocho = a.group_by {|i| i >= 6 }
+p uno
+p dos
+p tres
+p cuatro
+p cinco
+p seis
+p siete
+p ocho
